ee393172b760c2c8173275fd437ac7bd4394bd88
Teddy Yeh <tyeh26@gmail.com>
2017-06-17 13:28:56 -0700

    Setup Lobby UI

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
+import {List, ListItem} from 'material-ui/List';
 import {browserHistory} from 'react-router';
 
 
@@ -19,25 +19,25 @@ export default class LobbyView extends React.Component {
     }
 
     componentDidMount() {
-        let userId = localStorage.getItem('userId') || "";
+        let userId = localStorage.getItem('userId');
 
         if (!this.props.params.gameId || userId) {
             browserHistory.push("/");
         }
 
-        if(userId) {
-            this.setState({
-                userId: user_id,
-            });
 
-            {/* Eventually, query Lobby or Game State  */}
-            fetch(`/api/gamestatus?userId=${this.state.userId}`)
-            .then(response => {
-                return response.json()
-            }).then(j => {
-                this.updateLobbyState(j)
-            });
-        }
+        this.setState({
+            userId: userId,
+        });
+
+        {/* Eventually, query Lobby or Game State  */}
+        fetch(`/api/gamestatus?userId=${this.state.userId}`)
+        .then(response => {
+            return response.json()
+        }).then(j => {
+            this.updateLobbyState(j)
+        });
+
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.updateLobbyState = this.updateLobbyState.bind(this);
@@ -68,11 +68,14 @@ export default class LobbyView extends React.Component {
     render() {
         return (
             <div className="host-view">
+                <List>
+                    <ListItem primaryText="John" secondaryText="Host" />
+                    <ListItem primaryText="foobar" />
+                    <ListItem primaryText="Little Bobby Tables" />
+                </List>
                 <form onSubmit={this.handleSubmit}>
-                    <div><TextField hintText="A Game Id" value={this.state.gameId} onChange={this.handleGameIdChange} fullWidth={true} /></div>
-                    <div><TextField hintText="A Name" value={this.state.name} onChange={this.handleNameChange} fullWidth={true} /></div>
-
                     { this.state.isHost ? <div><RaisedButton type="submit" primary={true} label="start" fullWidth={true} /></div> : null }
+                    <div><RaisedButton type="submit" secondary={false} label="exit" fullWidth={true} /></div>
                 </form>
             </div>
         );
