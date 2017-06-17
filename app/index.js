39b2a682b10114f816180cf7def9643bbadd26f1
Amira Anuar <amiraanuar@gmail.com>
2017-06-16 20:46:19 -0700

    backend changes

--- app/index.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env nodejs
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Amira\n');
-}).listen(8080, 'localhost');
-console.log('Server running at http://localhost:8080/');
